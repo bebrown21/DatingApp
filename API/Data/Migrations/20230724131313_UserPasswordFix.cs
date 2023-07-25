e86484d2747ab2b8af793e1dec0a2a6f0ceb2436
Brandon Brown <brandonbrownb2@gmail.com>
2023-07-25 15:12:45 -0500

    add user account controller

---
+++ API/Data/Migrations/20230724131313_UserPasswordFix.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class UserPasswordFix : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "PasswordHast",
+                table: "Users",
+                newName: "PasswordHash");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "PasswordHash",
+                table: "Users",
+                newName: "PasswordHast");
+        }
+    }
+}
